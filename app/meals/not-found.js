4cf2ddffb8dda7842f37df6f0568ff9c6a42910b
Rokas <rokas.vaitkevicius1@gmail.com>
2024-02-05 17:24:05 +0200

    Added a not found page to meals directory

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>Couldnt find page you were looking for</p>
+    </main>
+  )
+}
